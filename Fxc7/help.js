6c0c773f5753574544cf7cf12c30c92625dc5f7b
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-02-26 23:02:33 +0700

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -49,32 +49,15 @@ const donasi = (name) => {
 exports.donasi = donasi
 // iklan menu
 const menu = (name) => {
-	return `╭──────「 *_ᴀᴅɪᴛɪʏᴀ ꜱᴛᴏʀᴇ_* 」
+	return `╭───────────「𝙼𝚎𝚐𝚞𝚖𝚒𝚗𝙱𝙾𝚃」
 ┴
-│🔰 _*UNCHEK GG BY ADITIYA STORE*_  🔰
-│
-│ *📧 20 UNCHEK : 10K*
-│ *📧 30 UNCHEK : 15K*
-│ *📧 40 UNCHEK : 20K*
-│ *📧 50 UNCHEK : 25K*
-│ *📧 55 UNCHEK : 30K*
-│ *📧 75 UNCHEK : 35K*
-│ _*DAN SETERUSNYA...*_
-│
-│ _*TAKE / BELI BANYAK KEUNTUNGAN HOKI LEBIH BESAR*_
-│
-│ *GAK DAPAT WE KASIH LAGI 1 ×*
-│
-│ *KHUSUS TANGGAL 24 - 26 , PEMBELIAN 30K DAPAT 200 UNCHEK*
-│
-│ *T E S T I ? ADA HAMPIR BEJIBUN + PUSH TESTI*
-│
-│ _*PAYMENT ?*_
-│ *- G O P A Y💸*
-│
-│https://chat.whatsapp.com/Kmn8Q3Az0CDFEOBJHPyBSh
+┣⊱  ❌ SPAM
+┣⊱  ❌ CALL & VC
+┣⊱  Melanggar?? Banned + Out Group*
+┣⊱  Link Gc: https://chat.whatsapp.com/LIWm11ke9836k38SIaKlFr
+┣⊱  Owner: Fajar Alfarizi
 ┬
-╰──────「 *_ɪᴋʟᴀɴ_* 」
+╰──────────────────────
 `
 }
 exports.menu = menu
